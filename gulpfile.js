d499a5222dc24d695b2747c750a7b09d9cfd0e0b
Anastasiia Kadurkina <kadurkinaanastasiia@yahoo.com>
2017-06-14 02:37:03 +0300

    Template gulp project

---
+++ gulpfile.js
@@ -0,0 +1,102 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    sync = require('browser-sync'),
+    cssnano = require('gulp-cssnano'), 
+    rename  = require('gulp-rename'), 
+    uglify = require('gulp-uglifyjs'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'), 
+    pngquant = require('imagemin-pngquant'),
+    cache = require('gulp-cache'),
+    autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function(){    //command not found
+   return gulp.src('app/scss/*.scss') // ('app/scss/main.+(scss|sass)')
+   .pipe(sass().on('error', sass.logError))
+   .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+   .pipe(concat('style.css'))
+   .pipe(gulp.dest('dist/css'))
+   .pipe(sync.reload({stream: true}))
+});
+
+gulp.task('just-reload', function(){
+    sync.reload();
+});
+
+
+gulp.task('sync', function(){
+   sync({
+       server: {
+           baseDir: 'dist'
+       }
+   }); 
+});
+
+
+gulp.task('scripts', function() {
+    return gulp.src([
+        'app/libs/jquery/dist/jquery.min.js', // Берем jQuery
+        'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js' 
+        ])
+        .pipe(concat('libs.min.js')) 
+        .pipe(uglify()) 
+        .pipe(gulp.dest('app/js')); 
+});
+
+gulp.task('css-libs', ['sass'], function() {
+    return gulp.src('app/css/libs.css') 
+        .pipe(cssnano()) 
+        .pipe(rename({suffix: '.min'})) 
+        .pipe(gulp.dest('app/css')); 
+});
+
+gulp.task('watch', ['sync', 'sass', 'css-libs', 'scripts'], function(){
+    gulp.watch('app/scss/*.scss', ['sass']);
+    gulp.watch('dist/index.html', ['just-reload']);
+    gulp.watch('*.js', ['just-reload']);
+});
+
+
+
+gulp.task('clean', function() {
+    return del.sync('dist');
+});
+
+
+gulp.task('img', function() {
+    return gulp.src('app/img/**/*') 
+        .pipe(cache(imagemin({
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        })))
+        .pipe(gulp.dest('dist/img'));
+});
+
+
+gulp.task('build', ['clean', 'img', 'sass', 'scripts'], function(){
+
+    var buildCss = gulp.src([ 
+        'app/css/main.css',
+        'app/css/libs.min.css'
+        ])
+    .pipe(gulp.dest('dist/css'))
+
+    var buildFonts = gulp.src('app/fonts/**/*') 
+    .pipe(gulp.dest('dist/fonts'))
+
+    var buildJs = gulp.src('app/js/**/*') 
+    .pipe(gulp.dest('dist/js'))
+
+    var buildHtml = gulp.src('app/*.html') 
+    .pipe(gulp.dest('dist'));
+
+});
+    
+    gulp.task('clear', function () {
+    return cache.clearAll();
+})
+    
+    gulp.task('default', ['watch']);
